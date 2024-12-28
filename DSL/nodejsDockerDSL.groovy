6030dc9fe9de9d82292fcd23fb46b03beb39598d
ItsBlaZZe <115127110+ItsBlaZZe@users.noreply.github.com>
2024-12-27 22:35:14 -0600

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -2,8 +2,8 @@ job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
         git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+            node / gitConfigName('ItsBlaZZe')
+            node / gitConfigEmail('sebas.cabezas3@hotmail.com')
         }
     }
     triggers {
@@ -14,9 +14,9 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('ItsBlaZZe/nodejsapp')
             tag('${GIT_REVISION,length=7}')
-            registryCredentials('docker-hub')
+            registryCredentials('burningblazze')
             forcePull(false)
             createFingerprints(false)
             skipDecorate()
